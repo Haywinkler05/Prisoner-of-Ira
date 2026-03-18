64ed54e1b653c5e89151d0b07c3321775421d1ec
Haywinker05 <haywinkler05@gmail.com>
2026-03-18 16:06:56 -0500

    [Feat] Enemy Class expanded
    Added getters, setters, and common functions

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,5 +5,26 @@ public abstract class Enemy : MonoBehaviour
     [SerializeField] private float _health;
     [SerializeField] private float _maxHealth;
     [SerializeField] private float _damage;
+    [SerializeField] private float _maxDamage;
     [SerializeField] private float _moveSpeed;
+
+
+
+    public float Health { get { return _health; } set {  _health = value; } }
+    public float MaxHealth { get { return _maxHealth; } set { _maxHealth = value; } }
+    public float Damage { get { return _damage; } set { _damage = value; } }
+    public float MaxDamage { get { return _maxDamage; } set { _maxDamage = value; } }
+    public float MoveSpeed { get { return _moveSpeed; } set { _moveSpeed = value; } } 
+
+
+    public void takeDamage(float incomingDamage)
+    {
+        _health -= incomingDamage;
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
 }
